83d993c84c16d02920382a530962230f95189260
Shaikram <shaikram02@gmail.com>
2022-07-09 15:48:08 -0800

    c1

---
+++ version control/file1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Professional Version Control</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+  </head>
+  <body>
+    <ol>
+      <li>Mercury</li>
+      <li>Venus</li>
+      <li>Earth</li>
+      <li>Mars</li>
+    </ol>
+
+  </body>
+</html>
