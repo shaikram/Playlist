b640962297f501cba5fda1cdeb39935eb9bf62cb
Aikee <shaikram022@gmail.com>
2022-07-11 10:59:15 -0800

    Adding Style

--- list.php
+++ list.php
@@ -2,15 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Git and Guthub</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
   </head>
   <body>
     <center>
       <div class="header">
         <h1>HEADER</h1>
-        <div class="container">
-          <p>Container</p>
-        </div>
+      </div>
+      <div class="container">
+        <p>Container</p>
       </div>
     </center>
   </body>
