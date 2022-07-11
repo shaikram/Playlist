8edc8ec07717b64858b81afab3ec67ed2d3fb5de
Aikee <shaikram022@gmail.com>
2022-07-11 10:44:39 -0800

    HTML Tags

---
+++ list.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <center>
+      <div class="header">
+        <h1>HEADER</h1>
+        <div class="container">
+          <p>Container</p>
+        </div>
+      </div>
+    </center>
+  </body>
+</html>
